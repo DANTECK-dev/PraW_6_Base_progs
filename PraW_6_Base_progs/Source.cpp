78d35cf3ab962ad1a379b2f8f89a9277767b06f8
Александр Немеров <nemerov_a@mail.ru>
2021-12-15 17:42:17 +0800

    Добавьте файлы проекта.

---
+++ PraW_6_Base_progs/Source.cpp
@@ -0,0 +1,22 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#define N 10
+#define M 1000
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <conio.h>
+#include <time.h>
+#include <Windows.h>
+
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	/**/
+
+
+
+}
